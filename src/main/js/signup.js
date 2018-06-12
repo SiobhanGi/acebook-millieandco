6acef100994417784eaed2eacee98be20c398a27
William Powell <william.powell92@gmail.com>
2018-06-12 14:49:47 +0100

    refactored sign up

--- src/main/js/signup.js
+++ src/main/js/signup.js
@@ -1,70 +1,59 @@
 const React = require('react');
 const client = require('./client');
 
-
-
 class Signup extends React.Component {
-	  constructor(props) {
-        super(props)
-        this.state = {}
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-      }
+	constructor(props) {
+		super(props)
+		this.state = {}
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    createUser(user) {
-                client({
-                method: 'POST',
-                path: '/api/users',
-                entity: user,
-                headers: {'Content-Type': 'application/json'}
-                })}
+	createUser(user) {
+		client({
+			method: 'POST',
+			path: '/api/users',
+			entity: user,
+			headers: {'Content-Type': 'application/json'}
+		});
+	}
 
-      handleChange(event) {
-        const name = event.target.name
-        const value = event.target.value
+	handleChange(event) {
+		const name = event.target.name
+		const value = event.target.value
 
-        this.setState({
-        [name]: value
-        })
-        }
+		this.setState(
+			{ [name]: value }
+		);
+	}
 
+	handleSubmit(event) {
+		event.preventDefault();
+		var newUser = {};
+		newUser['firstName'] = this.state.firstName;
+		newUser['lastName'] = this.state.lastName;
+		newUser['email'] = this.state.email;
+		newUser['password'] = this.state.password;
 
-      handleSubmit(event) {
-        event.preventDefault();
-        var newUser = {};
-        newUser['firstName'] = this.state.firstName;
-        newUser['lastName'] = this.state.lastName;
-        newUser['email'] = this.state.email;
-        newUser['password'] = this.state.password;
-        this.createUser(newUser);
-
-
-
-
-
-      }
-
-
-
+		this.createUser(newUser);
+	}
 
 	render() {
-
 		return (
-           <div className="span3">
-              <h2>Sign Up</h2>
-              <form onSubmit={this.handleSubmit}>
-              <label>First Name</label>
-              <input type="text" name="firstName" onChange={this.handleChange} className = "first-name span3"/>
-              <label>Last Name</label>
-              <input type="text" name="lastName" onChange={this.handleChange} className = "last-name span3"/>
-              <label>Email Address</label>
-              <input type="email" name="email" onChange={this.handleChange} className = "email span3"/>
-              <label>Password</label>
-              <input type="password" name="password" onChange={this.handleChange} className = "password span3"/>
-              <input type="submit" value="Sign up" className = "submit btn btn-primary pull-right"/>
-              <div className="clearfix"></div>
-              </form>
-           </div>
+			<div className="span3">
+				<h2>Sign Up</h2>
+				<form onSubmit={this.handleSubmit}>
+					<label>First Name</label>
+					<input type="text" name="firstName" onChange={this.handleChange} className = "first-name span3"/>
+					<label>Last Name</label>
+					<input type="text" name="lastName" onChange={this.handleChange} className = "last-name span3"/>
+					<label>Email Address</label>
+					<input type="email" name="email" onChange={this.handleChange} className = "email span3"/>
+					<label>Password</label>
+					<input type="password" name="password" onChange={this.handleChange} className = "password span3"/>
+					<input type="submit" value="Sign up" className = "submit btn btn-primary pull-right"/>
+				</form>
+			</div>
 		)
 	}
 }

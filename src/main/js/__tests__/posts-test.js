4df4c156efc0d17a3699c6d9e95a81a581ab9d5d
William Powell <william.powell92@gmail.com>
2018-06-12 23:59:35 +0100

    tested react component posts

--- src/main/js/__tests__/posts-test.js
+++ src/main/js/__tests__/posts-test.js
@@ -6,8 +6,27 @@ import Posts from '../posts/posts';
 
 Enzyme.configure({adapter: new Adapter()});
 
+class FakePost extends React.Component {
+  render() {
+    return(
+      <div>Test</div>
+    );
+  }
+}
+
+Posts.__Rewire__('Post', FakePost)
+
 describe('<Posts />', () => {
   it('displays a title', () => {
+    const posts = shallow(<Posts />);
 
+    expect(posts.text()).toEqual('Posts');
+  });
+
+  it('renders multiple posts', () => {
+    const post = {_links: {self: {href: 'test'}}}
+    const posts = shallow(<Posts posts={ [post, post, post] } />);
+
+    expect(posts.find(FakePost).length).toEqual(3);
   });
 });

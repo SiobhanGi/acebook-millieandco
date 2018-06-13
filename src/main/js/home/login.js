ebbf863175bd1f7193f74e4646cbc13b3a30d928
John Newman <newmanj903@gmail.com>
2018-06-13 14:28:00 +0100

    test coverage for login

--- src/main/js/home/login.js
+++ src/main/js/home/login.js
@@ -1,32 +1,32 @@
 import React from 'react';
 
-    const defaultProps = {
-        text: ""
-    };
+   const defaultProps = {
+       text: ""
+   };
 
- class Login extends React.Component {
-  render() {
-    return (
-          <div className='login'>
-            <div className='login_inner'>
-            <button onClick={this.props.closeLogin}>X</button>
-              <div className="row">
-                <div className="col-xs-8 col-xs-offset-2">
-                <form className="form-group">
-                  <label>Email:</label>
-                  <input className="form-control" type="text" />
-                  <br />
-                  <label>Password:</label>
-                  <input className="form-control" type="password" />
-                  <br />
-                  <input type="submit" value="submit" />
-                </form>
-                </div>
-              </div>
-            </div>
-          </div>
-    );
-  }
+class Login extends React.Component {
+ render() {
+   return (
+         <div className='login'>
+           <div className='login_inner'>
+           <button onClick={this.props.closeLogin}>X</button>
+             <div className="row">
+               <div className="col-xs-8 col-xs-offset-2">
+               <form className="form-group">
+                 <label>Email:</label>
+                 <input className="form-control" type="text" />
+                 <br />
+                 <label>Password:</label>
+                 <input className="form-control" type="password" />
+                 <br />
+                 <input type="submit" value="submit" />
+               </form>
+               </div>
+             </div>
+           </div>
+         </div>
+   );
+ }
 }
 
 export default Login;

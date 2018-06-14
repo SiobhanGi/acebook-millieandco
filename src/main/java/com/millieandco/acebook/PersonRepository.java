d6f98d6e71a0fe4431c7df807d80f341f46cf8c5
Charlene <charlenechetcuti@gmail.com>
2018-06-14 11:24:05 +0100

    add log out

--- src/main/java/com/millieandco/acebook/PersonRepository.java
+++ src/main/java/com/millieandco/acebook/PersonRepository.java
@@ -1,11 +1,15 @@
 package com.millieandco.acebook;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-//@RepositoryRestResource(exported = false)
+@RepositoryRestResource
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    @Override
+    @RestResource(exported = false)
     List<Person> findAll();
 
     Person save(Person person);
